02f098c9321700bd48acabd7b3a4ae265d670d93
it24hdev <https://github.com/it24hdev>
2022-08-06 09:44:40 +0700

    no message

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Config;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
