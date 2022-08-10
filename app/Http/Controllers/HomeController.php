eaf3a8d4a3b2578f18d7a3ef60bd6aa16de2f089
it24hdev <https://github.com/it24hdev>
2022-08-10 14:47:34 +0700

    no message

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,94 +12,46 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $danhsachdichvu_footer  = Post::where('status' , '=' , 1)
+        $danhsachdichvu  = Post::where('status' , '=' , 1)
                                 ->where('service', '=' , 1)
                                 ->orderby('id','asc')->limit(5)->get();
-        $danhsachbaiviet_footer = Post::where('status' , '=' , 1)
+        $danhsachbaiviet = Post::where('status' , '=' , 1)
                                 ->where('service', '=' , 2)
                                 ->orderby('created_at','desc')->limit(3)->get();
         $danhmucbaiviet  = Category::where('status','=',1)->orderby('id','asc')->get();
         $sliders = DB::table('sliders')->where('location',2)->where('status', 1)->orderBy('position', 'ASC')->get();
         return view('frontend.index',
             [
-                'danhmucbaiviet'          => $danhmucbaiviet,
-                'sliders'                 => $sliders,
-                'danhsachdichvu_footer'   => $danhsachdichvu_footer,
-                'danhsachbaiviet_footer'  => $danhsachbaiviet_footer,
+                'danhmucbaiviet'  => $danhmucbaiviet,
+                'sliders'         => $sliders,
+                'danhsachdichvu'  => $danhsachdichvu,
+                'danhsachbaiviet'  => $danhsachbaiviet,
             ]);
     }
     public function list_post(){
-
-        $danhsachdichvu_footer  = Post::where('status' , '=' , 1)
-                                ->where('service', '=' , 1)
-                                ->orderby('id','asc')->limit(5)->get();
-        $danhsachbaiviet_footer = Post::where('status' , '=' , 1)
-                                ->where('service', '=' , 2)
-                                ->orderby('created_at','desc')->limit(3)->get();
-
-
         $danhmucbaiviet = Category::where('status','=',1)->orderby('id','asc')->get();
-        return \view('frontend.list-post',[
-            'danhmucbaiviet'          => $danhmucbaiviet,
-            'danhsachdichvu_footer'   => $danhsachdichvu_footer,
-            'danhsachbaiviet_footer'  => $danhsachbaiviet_footer,
-        ]);
+        return \view('frontend.list-post',[  'danhmucbaiviet'  => $danhmucbaiviet]);
     }
     public function list_service(){
-
-        $danhsachdichvu_footer  = Post::where('status' , '=' , 1)
-                                ->where('service', '=' , 1)
-                                ->orderby('id','asc')->limit(5)->get();
-        $danhsachbaiviet_footer = Post::where('status' , '=' , 1)
-                                ->where('service', '=' , 2)
-                                ->orderby('created_at','desc')->limit(3)->get();
         $danhsachdichvu = Post::where('status' , '=' , 1)
                                 ->where('service', '=' , 1)
                                 ->orderby('id','asc')->get();
         $danhmucbaiviet = Category::where('status','=',1)->orderby('id','asc')->get();
         return \view('frontend.list-service',
             [ 
-                'danhmucbaiviet'          => $danhmucbaiviet,
-                'danhsachdichvu'          => $danhsachdichvu,
-                'danhsachdichvu_footer'   => $danhsachdichvu_footer,
-                'danhsachbaiviet_footer'  => $danhsachbaiviet_footer,
+                'danhmucbaiviet'  => $danhmucbaiviet,
+                'danhsachdichvu'  => $danhsachdichvu,
             ]);
     }
     public function contact(){
-        $danhsachdichvu_footer  = Post::where('status' , '=' , 1)
-                                ->where('service', '=' , 1)
-                                ->orderby('id','asc')->limit(5)->get();
-        $danhsachbaiviet_footer = Post::where('status' , '=' , 1)
-                                ->where('service', '=' , 2)
-                                ->orderby('created_at','desc')->limit(3)->get();
         $danhmucbaiviet = Category::where('status','=',1)->orderby('id','asc')->get();
-        return \view('frontend.contact',[
-            'danhmucbaiviet'          => $danhmucbaiviet,
-            'danhsachdichvu_footer'   => $danhsachdichvu_footer,
-            'danhsachbaiviet_footer'  => $danhsachbaiviet_footer,
-        ]);
+        return \view('frontend.contact',[  'danhmucbaiviet'  => $danhmucbaiviet]);
     }
     public function detail_post(){
-        $danhsachdichvu_footer  = Post::where('status' , '=' , 1)
-                                ->where('service', '=' , 1)
-                                ->orderby('id','asc')->limit(5)->get();
-        $danhsachbaiviet_footer = Post::where('status' , '=' , 1)
-                                ->where('service', '=' , 2)
-                                ->orderby('created_at','desc')->limit(3)->get();
         $danhmucbaiviet = Category::where('status','=',1)->orderby('id','asc')->get();
-        return \view('frontend.detail-post',[
-            'danhmucbaiviet'          => $danhmucbaiviet,
-            'danhsachdichvu_footer'   => $danhsachdichvu_footer,
-            'danhsachbaiviet_footer'  => $danhsachbaiviet_footer,
-        ]);
+        return \view('frontend.detail-post',[  'danhmucbaiviet'  => $danhmucbaiviet]);
     }
     public function detail_service($slug){
-        $danhsachdichvu_footer  = Post::where('status' , '=' , 1)
-                                ->where('service', '=' , 1)
-                                ->orderby('id','asc')->limit(5)->get();
-        $danhsachbaiviet_footer = Post::where('status' , '=' , 1)
-                                ->where('service', '=' , 2)
-                                ->orderby('created_at','desc')->limit(3)->get();
         $chitietdichvu  = Post::where('slug','=',$slug)->first();
         $danhsachdichvu = Post::where('status' , '=' , 1)
                                 ->where('service', '=' , 1)
